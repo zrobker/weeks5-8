bba911ccad8bffee76aa6d94b87624353234fe63
Zach Robker <zrobker@byui.edu>
2023-05-25 10:17:20 -0400

    new project

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+// const db = require('./models');
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`DB Connected and server running on ${port}.`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Cannot connect to the database!', err);
+//     process.exit();
+//   });
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
