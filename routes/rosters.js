6dff89476ec92c820382b599d8f2031483300bc6
Zach Robker <zrobker@byui.edu>
2023-06-10 20:41:32 -0400

    OAuth

--- routes/rosters.js
+++ routes/rosters.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 const rostersController = require('../controllers/rosters');
 const validation = require('../middleware/validate.js');
+const { requiresAuth } = require('express-openid-connect');
 
 router.get('/', rostersController.getAll);
 
 router.get('/:id', rostersController.getSingle);
 
-router.post('/', validation.saveRoster, rostersController.createRoster);
+router.post('/', requiresAuth, validation.saveRoster, rostersController.createRoster);
 
-router.put('/:id', validation.saveRoster, rostersController.updateRoster);
+router.put('/:id', requiresAuth, validation.saveRoster, rostersController.updateRoster);
 
-router.delete('/:id', rostersController.deleteRoster);
+router.delete('/:id', requiresAuth, rostersController.deleteRoster);
 
 module.exports = router;
