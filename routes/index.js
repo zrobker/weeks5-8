bfa059b5c56c9ee917680d5612ae1da5b5d8ebe7
Zach Robker <zrobker@byui.edu>
2023-06-10 22:03:27 -0400

    requireAuth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
 
-router.use('/', require('./swagger'));
-router.use('/teams', require('./teams'));
-router.use('/rosters', require('./rosters'));
+router.use('/',requiresAuth(), require('./swagger'));
+router.use('/teams',requiresAuth(), require('./teams'));
+router.use('/rosters',requiresAuth(), require('./rosters'));
 
 module.exports = router;
