34fbfdba8be10bb8b4d06ad1f2546e20c6126720
Zach Robker <zrobker@byui.edu>
2023-06-10 22:07:52 -0400

    undo agian

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
 router.use('/', require('./swagger'));
-router.use('/teams',requiresAuth(), require('./teams'));
-router.use('/rosters',requiresAuth(), require('./rosters'));
+router.use('/teams', require('./teams'));
+router.use('/rosters', require('./rosters'));
 
 module.exports = router;
