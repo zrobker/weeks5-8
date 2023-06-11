8d1057c833d26ad32c034267e98da256d05e1607
Zach Robker <zrobker@byui.edu>
2023-06-10 22:05:25 -0400

    undo

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
-router.use('/',requiresAuth(), require('./swagger'));
+router.use('/', require('./swagger'));
 router.use('/teams',requiresAuth(), require('./teams'));
 router.use('/rosters',requiresAuth(), require('./rosters'));
 
